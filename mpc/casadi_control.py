e4252b52fa0e5fec56ad5c6068bb7ccfcd6cf732
Rahel Rickenbach <rrahel@ethz.ch>
2024-05-15 17:03:19 +0200

    adapted casadi control file

--- mpc/casadi_control.py
+++ mpc/casadi_control.py
@@ -28,8 +28,8 @@ class CasadiControl():
     def __init__(self, track_coordinates=None, params=None):
         super().__init__()
 
-        # states: sigma, d, phi, v (4) + d_lb, d_ub (2)
-        self.n_state = 4 + 2           # here add amount of states plus amount of exact penalty terms
+        # states: sigma, d, phi, v (4) + sigma_0, sigma_diff (2) + d_lb, d_ub (2) + d_pen (1) + v_ub (1)
+        self.n_state = 4 + 2 + 1 +1         # here add amount of states plus amount of exact penalty terms
         # control: a, delta
         self.n_ctrl = 2
 
@@ -57,16 +57,16 @@ class CasadiControl():
             self.params = params
             assert len(self.params) == 2
 
-            self.delta_threshold_rad = np.pi  #12 * 2 * np.pi / 360
-            self.d_threshold = 2
-            self.max_acceleration = 2
+        self.delta_threshold_rad = np.pi  #12 * 2 * np.pi / 360
+        self.v_max = 2
+        self.max_acceleration = 2
 
-            self.dt = 0.05   # name T in document
+        self.dt = 0.05   # name T in document
 
-            self.track_width = 0.5
+        self.track_width = 0.5
 
-            self.lower = -self.track_width/2
-            self.upper = self.track_width/2
+        self.lower = -self.track_width/2
+        self.upper = self.track_width/2
 
     def sigmoid(self, x):
       	return 1 / (1 + exp(-x))
@@ -107,13 +107,29 @@ class CasadiControl():
         print("curv end")
         return curv
 
-    def mpc_casadi(self,q,p,x0,horizon,df,dc,dx,du,track_width,v_max):
+    def penalty_d(self, d, factor=100000.):
+        overshoot_pos = np.clip((d - 0.5*self.track_width*0.75),0,None)
+        overshoot_neg = np.clip((-d - 0.5*self.track_width*0.75),0,None)
+        penalty_pos = np.exp(overshoot_pos) - 1
+        penalty_neg = np.exp(overshoot_neg) - 1
+        return factor*(penalty_pos + penalty_neg)
+
+    def penalty_v(self, v, factor=100000.):
+        penalty_pos = np.clip((v - self.v_max*0.95),0,None) ** 2
+        return factor*penalty_pos
+
+    def mpc_casadi(self,q,p,x0,horizon,df,dc,dx,du):
 
         # with: dx, du: number of states/inputs passed from the dynamics
         # dc: number of constraints
         # df: number of states that we do not use
+        q_used = np.hstack([q[5],q[1:4],q[6:]])
+        p_used = np.hstack([p[5],p[1:4],p[6:]])
 
-        Ts = 0.05
+        # here the q and the p scale the following
+        # feature vector [sigma-sigma_0, d, phi, v, penalty_d,penalty_v,a,delta]
+
+        Ts = self.dt
         N=horizon
         l_r=0.2
         l_f=0.2
@@ -131,19 +147,19 @@ class CasadiControl():
         beta = np.arctan(l_r/(l_r+l_f)*np.tan(u_sym[1,0:N]))
 
         dyn1 = horzcat((x_sym[0,0] - x0[0,0]), (x_sym[0,1:N+1] - x_sym[0,0:N] - Ts*(x_sym[3,0:N]*(np.cos(x_sym[2,0:N]+beta)/(1.-self.curv_casadi(x_sym[0,0:N])*x_sym[1,0:N])))))
-        dyn2 = horzcat((x_sym[1,0] - x0[0,1]), (x_sym[1,1:N+1] - x_sym[1,0:N] - Ts*(x_sym[3,0:N]*np.sin(x_sym[2,0:N])+beta)))
+        dyn2 = horzcat((x_sym[1,0] - x0[0,1]), (x_sym[1,1:N+1] - x_sym[1,0:N] - Ts*(x_sym[3,0:N]*np.sin(x_sym[2,0:N]+beta))))
         dyn3 = horzcat((x_sym[2,0] - x0[0,2]), (x_sym[2,1:N+1] - x_sym[2,0:N] - Ts*(x_sym[3,0:N]*(1/l_f)*np.sin(beta)-self.curv_casadi(x_sym[0,0:N])*x_sym[3,0:N]*(np.cos(x_sym[2,0:N]+beta)/(1-self.curv_casadi(x_sym[0,0:N])*x_sym[1,0:N])))))
         dyn4 = horzcat((x_sym[3,0] - x0[0,3]), (x_sym[3,1:N+1] - x_sym[3,0:N] - Ts*(u_sym[0,0:N])))
         # think about how to integrate the curvature function
 
         # define symbolic variables for cost parameters
-        feat = vertcat(x_sym[0,0:N]-x0[0,0],x_sym[1:,0:N],fmax(np.zeros([1,N]),(-x_sym[1,0:N]-track_width)),fmax(np.zeros([1,N]),(x_sym[1,0:N]-track_width)),fmax(np.zeros([1,N]),(-x_sym[3,0:N] + 0)),fmax(np.zeros([1,N]),(x_sym[3,0:N]-v_max)),u_sym[:,0:N])
+        feat = vertcat(x_sym[0,0:N]-x0[0,0],x_sym[1:,0:N],self.penalty_d(x_sym[1,0:N]),fmax(np.zeros([1,N]),(x_sym[1,0:N]-track_width)),self.penalty_v(x_sym[3,0:N]),u_sym[:,0:N])
         q_sym = SX.sym('q_sym',dx+dc+du)
         p_sym = SX.sym('q_sym',dx+dc+du)
         Q_sym = diag(q_sym)
 
         l = sum2(transpose(diag(transpose(feat)@Q_sym@feat)) + transpose(p_sym)@feat)
-        dl = substitute(substitute(l,q_sym,q),p_sym,p)
+        dl = substitute(substitute(l,q_sym,q_used),p_sym,p_used)
 
         const = vertcat(transpose(dyn1),transpose(dyn2),transpose(dyn3),transpose(dyn4),transpose(u_sym[0,0:N]),transpose(u_sym[1,0:N]))
         lbg = np.r_[np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),-2*np.ones(N),-1*np.ones(N)]
@@ -181,13 +197,18 @@ class CasadiControl():
         return sol_evalf
 
 
-    def mpc_casadi_with_constraints(self,q,p,x0,horizon,df,dc,dx,du,track_width,v_max):
+    def mpc_casadi_with_constraints(self,q,p,x0,horizon,df,dc,dx,du):
 
         # with: dx, du: number of states/inputs passed from the dynamics
         # dc: number of constraints
         # df: number of states that we do not use
+        q_used = np.hstack([q[5],q[1:4],q[8:]])
+        p_used = np.hstack([p[5],p[1:4],p[8:]])
 
-        Ts = 0.05
+        # here the q and the p scale the following
+        # feature vector [sigma-sigma_0, d, phi, v, penalty_d,penalty_v,a,delta]
+
+        Ts = self.dt
         N=horizon
         l_r=0.2
         l_f=0.2
@@ -205,7 +226,7 @@ class CasadiControl():
         beta = np.arctan(l_r/(l_r+l_f)*np.tan(u_sym[1,0:N]))
 
         dyn1 = horzcat((x_sym[0,0] - x0[0,0]), (x_sym[0,1:N+1] - x_sym[0,0:N] - Ts*(x_sym[3,0:N]*(np.cos(x_sym[2,0:N]+beta)/(1.-self.curv_casadi(x_sym[0,0:N])*x_sym[1,0:N])))))
-        dyn2 = horzcat((x_sym[1,0] - x0[0,1]), (x_sym[1,1:N+1] - x_sym[1,0:N] - Ts*(x_sym[3,0:N]*np.sin(x_sym[2,0:N])+beta)))
+        dyn2 = horzcat((x_sym[1,0] - x0[0,1]), (x_sym[1,1:N+1] - x_sym[1,0:N] - Ts*(x_sym[3,0:N]*np.sin(x_sym[2,0:N]+beta))))
         dyn3 = horzcat((x_sym[2,0] - x0[0,2]), (x_sym[2,1:N+1] - x_sym[2,0:N] - Ts*(x_sym[3,0:N]*(1/l_f)*np.sin(beta)-self.curv_casadi(x_sym[0,0:N])*x_sym[3,0:N]*(np.cos(x_sym[2,0:N]+beta)/(1-self.curv_casadi(x_sym[0,0:N])*x_sym[1,0:N])))))
         dyn4 = horzcat((x_sym[3,0] - x0[0,3]), (x_sym[3,1:N+1] - x_sym[3,0:N] - Ts*(u_sym[0,0:N])))
         # think about how to integrate the curvature function
@@ -217,11 +238,11 @@ class CasadiControl():
         Q_sym = diag(q_sym)
 
         l = sum2(transpose(diag(transpose(feat)@Q_sym@feat)) + transpose(p_sym)@feat)
-        dl = substitute(substitute(l,q_sym,q),p_sym,p)
+        dl = substitute(substitute(l,q_sym,q_used),p_sym,p_used)
 
         const = vertcat(transpose(dyn1),transpose(dyn2),transpose(dyn3),transpose(dyn4),transpose(u_sym[0,0:N]),transpose(u_sym[1,0:N]),transpose(x_sym[1,0:N+1]),transpose(x_sym[3,0:N+1]))
-        lbg = np.r_[np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),-2*np.ones(N),-1*np.ones(N),-track_width*np.ones(N+1),0*np.ones(N+1)]
-        ubg = np.r_[np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),2*np.ones(N),1*np.ones(N),track_width*np.ones(N+1),v_max*np.ones(N+1)]
+        lbg = np.r_[np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),-2*np.ones(N),-1*np.ones(N),-0.5*self.track_width*0.75*np.ones(N+1),-0.1*np.ones(N+1)]
+        ubg = np.r_[np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),2*np.ones(N),1*np.ones(N),0.5*self.track_width*0.75*np.ones(N+1),self.v_max*0.95*np.ones(N+1)]
         lbx = -np.inf * np.ones(dx*(N+1)+du*N)
         ubx = np.inf * np.ones(dx*(N+1)+du*N)
 
