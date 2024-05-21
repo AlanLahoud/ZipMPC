b15ed6f37ee6aab8d7f49f9521a104bec0cafa3c
Rahel Rickenbach <rrahel@ethz.ch>
2024-05-21 09:50:35 +0200

    added dynamic bicycle model, new files, notebook not ready yet

--- mpc/casadi_control.py
+++ mpc/casadi_control.py
@@ -27,7 +27,7 @@ import os
 class CasadiControl():
     def __init__(self, track_coordinates, params):
         super().__init__()
-        
+
         # states: sigma, d, phi, v (4) + sigma_0, sigma_diff (2) + d_pen (1) + v_ub (1) + ac_ub (1)
         self.n_state = 4+2+1+1+1
         print(self.n_state)          # here add amount of states plus amount of exact penalty terms
@@ -50,20 +50,20 @@ class CasadiControl():
         self.curv_f = self.track_curv_diff[self.mask]
 
         self.params = params
-        
+
         self.l_r = params[0]
         self.l_f = params[1]
-        
+
         self.track_width = params[2]
-        
+
         self.delta_threshold_rad = np.pi
         self.v_max = params[3]
         self.ac_max = params[4]
         self.dt = params[5] #0.04
-        
+
         self.a_max = params[6]
         self.delta_max = params[7]
-        
+
 
     def sigmoid(self, x):
       	return 1 / (1 + exp(-x))
@@ -252,7 +252,7 @@ class CasadiControl():
             transpose(u_sym[1,0:N]),
             transpose(x_sym[1,0:N+1]),
             transpose(x_sym[3,0:N+1]))
-        
+
         lbg = np.r_[np.zeros(N+1),
                     np.zeros(N+1),
                     np.zeros(N+1),
@@ -261,7 +261,7 @@ class CasadiControl():
                     -self.delta_max*np.ones(N),
                     -0.5*self.track_width*0.75*np.ones(N+1),
                     -0.1*np.ones(N+1)]
-        
+
         ubg = np.r_[np.zeros(N+1),
                     np.zeros(N+1),
                     np.zeros(N+1),
@@ -270,7 +270,7 @@ class CasadiControl():
                     self.delta_max*np.ones(N),
                     0.5*self.track_width*0.75*np.ones(N+1),
                     self.v_max*np.ones(N+1)]
-        
+
         lbx = -np.inf * np.ones(dx*(N+1)+du*N)
         ubx = np.inf * np.ones(dx*(N+1)+du*N)
 
@@ -303,10 +303,10 @@ class CasadiControl():
 
         # print solution
         return sol_evalf
-    
-    
-    
-    
+
+
+
+
     def mpc_casadi_with_constraints_2(self, q, p, x0, horizon, df, dc, dx, du):
         Ts = self.dt
         N = horizon
@@ -415,3 +415,99 @@ class CasadiControl():
         # Print solution
         return sol_evalf
 
+    def mpc_casadi_with_constraints_paj(self,q,p,x0,horizon,df,dc,dx,du):
+
+        # with: dx, du: number of states/inputs passed from the dynamics
+        # dc: number of constraints
+        # df: number of states that we do not use
+        q_used = np.hstack([q[7],q[1:6],q[10:]])
+        p_used = np.hstack([p[7],p[1:6],p[10:]])
+
+        # here the q and the p scale the following
+        # feature vector [sigma-sigma_0, d, phi, v, penalty_d,penalty_v,a,delta]
+
+        Ts = self.dt
+        N=horizon
+        l_r=0.2
+        l_f=0.2
+
+        # set them like in learned dynamics
+        m = 0.5
+        g = 9.81
+        mu = 0.5
+        I_z = m*l_r*l_f  # this should be an approximation
+
+        B = 10.0
+        C = 1.9
+        D = 1.0
+
+        dx = dx - dc - df
+
+        x_sym = SX.sym('x_sym',dx,N+1)
+        u_sym = SX.sym('u_sym',du,N)
+
+        a_f = np.arctan((x_sym[5,0:N]  + l_f*x_sym[3,0:N])/np.absolute(x_sym[4,0:N]))-u_sym[1,0:N]
+        a_r = np.arctan((x_sym[5,0:N]  - l_f*x_sym[3,0:N])/np.absolute(x_sym[4,0:N]))
+
+        F_yf = -0.5*m*g*mu*(D*np.sin(C*np.arctan(B*a_f)))
+        F_yr = -0.5*m*g*mu*(D*np.sin(C*np.arctan(B*a_r)))
+
+        #solver parameters
+        options = {}
+        options['ipopt.max_iter'] = 20000
+        options['verbose'] = False
+
+        beta = np.arctan(l_r/(l_r+l_f)*np.tan(u_sym[1,0:N]))
+
+        dyn1 = horzcat((x_sym[0,0] - x0[0,0]), (x_sym[0,1:N+1] - x_sym[0,0:N] - Ts*((x_sym[4,0:N]*np.cos(x_sym[2,0:N])-x_sym[5,0:N]*np.sin(x_sym[2,0:N]))/(1.-self.curv_casadi(x_sym[0,0:N])*x_sym[1,0:N]))))
+        dyn2 = horzcat((x_sym[1,0] - x0[0,1]), (x_sym[1,1:N+1] - x_sym[1,0:N] - Ts*(x_sym[4,0:N]*np.cos(x_sym[2,0:N])+x_sym[5,0:N]*np.sin(x_sym[2,0:N]))))
+        dyn3 = horzcat((x_sym[2,0] - x0[0,2]), (x_sym[2,1:N+1] - x_sym[2,0:N] - Ts*(x_sym[3,0:N] - self.curv_casadi(x_sym[0,0:N])*(x_sym[4,0:N]*np.cos(x_sym[2,0:N])-x_sym[5,0:N]*np.sin(x_sym[2,0:N]))/(1-self.curv_casadi(x_sym[0,0:N])*x_sym[1,0:N]))))
+        dyn4 = horzcat((x_sym[3,0] - x0[0,3]), (x_sym[3,1:N+1] - x_sym[3,0:N] - Ts*(1/I_z*(l_f*F_yf -l_r*F_yr))))
+        dyn5 = horzcat((x_sym[4,0] - x0[0,4]), (x_sym[4,1:N+1] - x_sym[4,0:N] - Ts*(u_sym[0,0:N]+x_sym[3,0:N]*x_sym[5,0:N])))
+        dyn6 = horzcat((x_sym[5,0] - x0[0,5]), (x_sym[5,1:N+1] - x_sym[5,0:N] - Ts*(1/m*(F_yf*np.cos(u_sym[1,0:N])+F_yr)-x_sym[3,0:N]*x_sym[4,0:N])))
+        # think about how to integrate the curvature function
+
+        # define symbolic variables for cost parameters
+        feat = vertcat(x_sym[0,0:N]-x0[0,0],x_sym[1:,0:N],u_sym[:,0:N])
+        q_sym = SX.sym('q_sym',dx+du)
+        p_sym = SX.sym('q_sym',dx+du)
+        Q_sym = diag(q_sym)
+
+        l = sum2(transpose(diag(transpose(feat)@Q_sym@feat)) + transpose(p_sym)@feat)
+        dl = substitute(substitute(l,q_sym,q_used),p_sym,p_used)
+
+        const = vertcat(transpose(dyn1),transpose(dyn2),transpose(dyn3),transpose(dyn4),transpose(dyn5),transpose(dyn6),transpose(u_sym[0,0:N]),transpose(u_sym[1,0:N]),transpose(x_sym[1,0:N+1]),transpose(x_sym[4,0:N+1]))
+        lbg = np.r_[np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),-2*np.ones(N),-1*np.ones(N),-0.5*self.track_width*0.75*np.ones(N+1),-0.1*np.ones(N+1)]
+        ubg = np.r_[np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),2*np.ones(N),1*np.ones(N),0.5*self.track_width*0.75*np.ones(N+1),self.v_max*0.95*np.ones(N+1)]
+        lbx = -np.inf * np.ones(dx*(N+1)+du*N)
+        ubx = np.inf * np.ones(dx*(N+1)+du*N)
+
+        x = vertcat(reshape(x_sym[:,0:N+1],(dx*(N+1),1)),reshape(u_sym[:,0:N],(du*N,1)))
+
+        # define solver
+        nlp = {'x':x,'f':dl, 'g':const}
+        solver = nlpsol('solver','ipopt', nlp, options)
+
+        # create solver input
+        solver_input = {}
+        solver_input['lbx'] = lbx
+        solver_input['ubx'] = ubx
+        solver_input['lbg'] = lbg
+        solver_input['ubg'] = ubg
+
+        # solve optimization problem
+        solver_output = solver(**solver_input)
+
+        # process ouput
+        sol = solver_output['x']
+        sol_evalf = np.squeeze(evalf(sol))
+        u = sol_evalf[-du*N:]
+        x = sol_evalf[:-du*N]
+        #print(u)
+        #print(x)
+        u_applied = u[0:1]
+
+        #print(sol.solveroutput.info.lambda)
+
+        # print solution
+        return sol_evalf
---
+++ mpc/env_dx/frenet_dyn_bicycle.py
@@ -0,0 +1,194 @@
+import torch
+from torch.autograd import Function, Variable
+import torch.nn.functional as F
+from torch import nn
+from torch.nn.parameter import Parameter
+
+import numpy as np
+
+from mpc import util
+
+import os
+
+import shutil
+FFMPEG_BIN = shutil.which('ffmpeg')
+
+import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+plt.style.use('bmh')
+
+# import sys
+# from IPython.core import ultratb
+# sys.excepthook = ultratb.FormattedTB(mode='Verbose',
+#      color_scheme='Linux', call_pdb=1)
+
+class FrenetDynBicycleDx(nn.Module):
+    def __init__(self, track_coordinates=None, params=None):
+        super().__init__()
+
+        # states: sigma, d, phi, r, v_x, v_y (6) + sigma_0, sigma_diff (2) + d_pen (1) + v_ub (1)
+        self.n_state = 6+2+1+1
+        print(self.n_state)          # here add amount of states plus amount of exact penalty terms
+        # control: a, delta
+        self.n_ctrl = 2
+
+        self.track_coordinates = track_coordinates
+
+        # everything to calculate curvature
+        self.track_sigma = self.track_coordinates[2,:]
+        self.track_curv = self.track_coordinates[4,:]
+
+        self.track_curv_shift = torch.empty(self.track_curv.size())
+        self.track_curv_shift[1:] = self.track_curv[0:-1]
+        self.track_curv_shift[0] = self.track_curv[-1]
+        self.track_curv_diff = self.track_curv - self.track_curv_shift
+
+        self.mask = torch.where(torch.absolute(self.track_curv_diff) < 0.1, False, True)
+        self.sigma_f = self.track_sigma[self.mask]
+        self.curv_f = self.track_curv_diff[self.mask]
+
+        self.params = params
+
+        self.l_r = params[0]
+        self.l_f = params[1]
+
+        self.track_width = params[2]
+
+        self.delta_threshold_rad = np.pi
+        self.v_max = params[3]
+        self.ac_max = params[4]
+        self.dt = params[5] #0.04
+
+    def curv(self, sigma):
+        '''
+        This function can stay the same
+        '''
+
+        num_sf = self.sigma_f.size()
+        num_s = sigma.size()
+
+        sigma_f_mat = self.sigma_f.repeat(num_s[0],1)
+
+
+        sigma_shifted = sigma.reshape(-1,1) - sigma_f_mat
+        curv_unscaled = torch.sigmoid(5000*sigma_shifted)
+        curv = (curv_unscaled@(self.curv_f.reshape(-1,1))).type(torch.float)
+
+
+        return curv.reshape(-1)
+
+
+    def penalty_d(self, d, factor=100000.):
+        overshoot_pos = (d - 0.5*self.track_width*0.75).clamp(min=0)
+        overshoot_neg = (-d - 0.5*self.track_width*0.75).clamp(min=0)
+        penalty_pos = torch.exp(overshoot_pos) - 1
+        penalty_neg = torch.exp(overshoot_neg) - 1
+        return factor*(penalty_pos + penalty_neg)
+
+    def penalty_v(self, v, factor=100000.):
+        # as v_x defines the forward direction - this function is applied with
+        # respect to v_x.
+        penalty_pos = (v - self.v_max*0.95).clamp(min=0) ** 2
+        return factor*penalty_pos
+
+    def forward(self, state, u):
+        softplus_op = torch.nn.Softplus(20)
+        squeeze = state.ndimension() == 1
+        if squeeze:
+            state = state.unsqueeze(0)
+            u = u.unsqueeze(0)
+        if state.is_cuda and not self.params.is_cuda:
+            self.params = self.params.cuda()
+
+        l_r,l_f = torch.unbind(self.params)
+
+        a, delta = torch.unbind(u, dim=1)
+
+        sigma, d, phi, r, v_x, v_y, sigma_0, sigma_diff, d_pen, v_ub = torch.unbind(state, dim=1)
+        #fP(α) = D sin(C arctan(Bα)) B = 10.0, C = 1.9, D = 1.0
+        # αf = arctan( vy + lf ˙ψ|vx|)− δf
+        a_f = torch.atan((v_y + l_f*r)/torch.abs(v_x))-delta
+        a_r = torch.atan((v_y - l_f*r)/torch.abs(v_x))
+
+        m = 0.5
+        g = 9.81
+        mu = 0.5
+        I_z = m*l_r*l_f # this should be an approximation
+
+        B = 10.0
+        C = 1.9
+        D = 1.0
+
+        F_yf = -0.5*m*g*mu*(D*torch.sin(C*torch.atan(B*a_f)))
+        F_yr = -0.5*m*g*mu*(D*torch.sin(C*torch.atan(B*a_r)))
+
+        dsigma = (v_x*torch.cos(phi)-v_y*torch.sin(phi))/(1.-self.curv(sigma)*d)
+        dd = v_x*torch.cos(phi)+v_y*torch.sin(phi)
+        dphi = r-self.curv(sigma)*((v_x*torch.cos(phi)-v_y*torch.sin(phi))/(1.-self.curv(sigma)*d))
+        dr = 1/I_z*(l_f*F_yf -l_r*F_yr)
+        dv_x = a + r*v_y
+        dv_y = 1/m*(F_yf*torch.cos(delta)+F_yr)-r*v_x
+        #print(sigma)
+        #print(self.curv(sigma))
+
+        sigma = sigma + self.dt * dsigma
+        d = d + self.dt * dd
+        phi = phi + self.dt * dphi
+        r = r + self.dt * dr
+        v_x = v_x + self.dt * dv_x
+        v_y = v_y + self.dt * dv_y
+        sigma_0 = sigma_0                   # we need to carry it on
+        sigma_diff = sigma - sigma_0
+
+        d_pen = self.penalty_d(d)
+
+        v_ub = self.penalty_v(v_x)
+
+        #d_lb = softplus_op(-d - 0.5*self.track_width)
+        #d_ub = softplus_op(d - 0.5*self.track_width)
+        #v_lb = softplus_op(-v + 0)
+        #v_ub = softplus_op(v - self.v_max)
+
+        state = torch.stack((sigma, d, phi, r, v_x, v_y, sigma_0, sigma_diff, d_pen, v_ub), 1)
+
+        return state
+
+
+    # This function is for plotting
+    # def get_frame(self, state, ax=None):
+    #     state = util.get_data_maybe(state.view(-1))
+    #     assert len(state) == 10
+    #     sigma, d, phi, v, sigma_0, sigma_diff, d_pen, v_ub = torch.unbind(state, dim=1)
+    #     l_r,l_f = torch.unbind(self.params)
+    #
+    #     if ax is None:
+    #         fig, ax = plt.subplots(figsize=(6,6))
+    #     else:
+    #         fig = ax.get_figure()
+    #
+    #     # here I need to figure out what we would like to plot
+    #     ax.plot(d,d, color='k')
+    #     ax.set_xlim((-2, 2))
+    #     ax.set_ylim((-2, 2))
+    #     return fig, ax
+
+    def get_true_obj(self):
+        # dimensions adjusted
+
+    	# 0  	 1   2   3  4	 5	   6        7          8      9     10  11
+        # sigma, d, phi, r, v_x, v_y, sigma_0, sigma_diff, d_pen, v_ub	a   delta
+        q = torch.Tensor([ 0.,  2.,  1.,  0., 0.,   0., 0., 0., 0., 0., 1., 2.])
+        assert not hasattr(self, 'mpc_lin')
+        p = torch.Tensor([ 0.,  0.,  0.,  0., 0., 0., 0., -2., 100., 100., -1,  0.])
+        return Variable(q), Variable(p)
+
+if __name__ == '__main__':
+    dx = FrenetKinBicycleDx()
+    n_batch, T = 8, 50
+    u = torch.zeros(T, n_batch, dx.n_ctrl)
+    xinit = torch.zeros(n_batch, dx.n_state)
+    xinit= torch.Tensor([0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
+    x = xinit
+    for t in range(T):
+        x = dx(x, u[t])
