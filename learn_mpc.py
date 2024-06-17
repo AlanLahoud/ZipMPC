63bbc01b5d177a4d8bc6820a544634bc2791d219
Alan Lahoud <alan.lahoud@oru.se>
2024-06-17 15:56:04 +0200

    fixing sigma sample

--- learn_mpc.py
+++ learn_mpc.py
@@ -83,10 +83,10 @@ def sample_xinit(n_batch):
         r = high-low
         return torch.rand(shape)*r+low
     
-    sigma = uniform(n_batch, 0.01, 20.)
+    sigma = uniform(n_batch, 4.01, 4.51)
     d = uniform(n_batch, -track_width*0.35, track_width*0.35)
     phi = uniform(n_batch, -0.4*np.pi, 0.4*np.pi)
-    v = uniform(n_batch, .01, 0.95*v_max)
+    v = uniform(n_batch, .40, 0.95*v_max)
 
     sigma_0 = sigma
     sigma_diff = sigma-sigma_0
