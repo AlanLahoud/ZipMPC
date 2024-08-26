3c1b409d68b39208179beff9307b5a379f3ab7b0
Alan Lahoud <alan.lahoud@oru.se>
2024-08-26 20:26:41 +0200

    back ti normal sampling

--- train_cost_mpc.py
+++ train_cost_mpc.py
@@ -132,7 +132,7 @@ lqr_iter = 50
 grad_method = GradMethods.AUTO_DIFF
 
 model = utils_new.SimpleNN(mpc_H, n_Q, 3, max_p)
-opt = torch.optim.Adam(model.parameters(), lr=0.00003, weight_decay=1e-5)
+opt = torch.optim.Adam(model.parameters(), lr=0.00001, weight_decay=1e-5)
 #opt = torch.optim.RMSprop(model.parameters(), lr=0.0005)
 
 control = utils_new.CasadiControl(track_coord, params)
