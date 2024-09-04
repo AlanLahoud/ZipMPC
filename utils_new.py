b7a42a668f65e4883d9ad245179024f307cca0b5
Alan Lahoud <alan.lahoud@oru.se>
2024-09-04 16:04:34 +0200

    model mismatch

--- utils_new.py
+++ utils_new.py
@@ -247,8 +247,8 @@ def sample_init(BS, dyn, sn=None):
         gen.manual_seed(sn)
     
     di = 1000
-    sigma_sample = torch.randint(int(0.0*di), int(3.5*di), (BS,1), generator=gen)/di
-    d_sample = torch.randint(int(-.10*di), int(.10*di), (BS,1), generator=gen)/di
+    sigma_sample = torch.randint(int(0.0*di), int(2.5*di), (BS,1), generator=gen)/di
+    d_sample = torch.randint(int(-.03*di), int(.03*di), (BS,1), generator=gen)/di
     phi_sample = torch.randint(int(-0.08*di), int(0.08*di), (BS,1), generator=gen)/di
     v_sample = torch.randint(0, int(.5*di), (BS,1), generator=gen)/di
     
