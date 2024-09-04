5ca2440819f1cac02791c7f01c24305bc2f6638e
Alan Lahoud <alan.lahoud@oru.se>
2024-09-04 15:36:07 +0200

    model mismatch

--- train_cost_mpc.py
+++ train_cost_mpc.py
@@ -134,7 +134,7 @@ lqr_iter = 50
 grad_method = GradMethods.AUTO_DIFF
 
 model = utils_new.SimpleNN(mpc_H, n_Q, 4, max_p)
-opt = torch.optim.Adam(model.parameters(), lr=0.00005, weight_decay=1e-5)
+opt = torch.optim.Adam(model.parameters(), lr=0.0002, weight_decay=1e-5)
 #opt = torch.optim.RMSprop(model.parameters(), lr=0.0005)
 
 control = utils_new.CasadiControl(track_coord, params)
