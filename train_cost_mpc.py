c964937fac897129f9e533eff24b6a92de3327fb
Alan Lahoud <alan.lahoud@oru.se>
2024-09-09 20:01:36 +0200

    eval lap

--- train_cost_mpc.py
+++ train_cost_mpc.py
@@ -195,7 +195,7 @@ def sample_x0_from_buffer(BS, buffer_x0):
     x0_sample = buffer_x0[nearest_indices]
 
     #idxs = torch.randint(0, len(buffer_x0), (BS,))
-    x0_sample = buffer_x0[idxs]
+    #x0_sample = buffer_x0[idxs]
     return x0_sample
 
 
