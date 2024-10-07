e9267f24e423f438c24f34be87741db3b542766d
Alan Lahoud <alan.lahoud@oru.se>
2024-10-07 11:40:15 +0200

    adjusting random params in sampling (more variation allowed)

--- utils_new.py
+++ utils_new.py
@@ -376,9 +376,9 @@ def sample_init_traj_dist(BS, dyn, traj, num_patches, sn=None):
     # Note that we clamp the sampled d and v values to stay in their constraints. Sampling constraint violating
     # states would not make sense.
 
-    d_sample = torch.clamp(torch.from_numpy(traj_sample[:,1].reshape(-1,1))+torch.randint(int(-.01*di), int(.01*di), (BS,1), generator=gen)/di,-0.24,0.24)
-    phi_sample = torch.from_numpy(traj_sample[:,2].reshape(-1,1))+torch.randint(int(-0.01*di), int(0.01*di), (BS,1), generator=gen)/di
-    v_sample = torch.clamp(torch.from_numpy(traj_sample[:,3].reshape(-1,1))+torch.randint(int(-0.01*di), int(.01*di), (BS,1), generator=gen)/di,0.0,2.5)
+    d_sample = torch.clamp(torch.from_numpy(traj_sample[:,1].reshape(-1,1))+torch.randint(int(-.03*di), int(.03*di), (BS,1), generator=gen)/di,-0.22,0.22)
+    phi_sample = torch.from_numpy(traj_sample[:,2].reshape(-1,1))+torch.randint(int(-0.03*di), int(0.03*di), (BS,1), generator=gen)/di
+    v_sample = torch.clamp(torch.from_numpy(traj_sample[:,3].reshape(-1,1))+torch.randint(int(-0.1*di), int(0.1*di), (BS,1), generator=gen)/di,0.0,1.5)
 
     # and this part we can actually keep
 
