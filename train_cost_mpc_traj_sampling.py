0e902a82ab6e6662cf06d430b3f0d8bb0136ec66
Alan Lahoud <alan.lahoud@oru.se>
2024-10-09 13:59:18 +0200

    traj sampling version

--- train_cost_mpc_traj_sampling.py
+++ train_cost_mpc_traj_sampling.py
@@ -126,7 +126,7 @@ u0 = torch.tensor([0.0, 0.0])
 dx=4
 du=2
 
-BS = 128
+BS = 64
 u_lower = torch.tensor([-a_max, -delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
 u_upper = torch.tensor([a_max, delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
 u_init= torch.tensor([0.1, 0.0]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(device)
@@ -136,7 +136,7 @@ lqr_iter = 70
 grad_method = GradMethods.AUTO_DIFF
 
 model = utils_new.SimpleNN(mpc_H, n_Q, 3, max_p)
-opt = torch.optim.Adam(model.parameters(), lr=0.00001, weight_decay=1e-5)
+opt = torch.optim.Adam(model.parameters(), lr=0.00005, weight_decay=1e-5)
 #opt = torch.optim.RMSprop(model.parameters(), lr=0.0005)
 
 control = utils_new.CasadiControl(track_coord, params)
@@ -232,7 +232,7 @@ best_prog = -999999.
 #x0 = torch.tensor([0.0, 0.1, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0])
 #u0 = torch.tensor([0.0, 0.0])
 
-num_traj_updates = 10
+epochs = 50
 num_patches = 10
 BS_init = 40
 BS_val = 10
@@ -295,248 +295,146 @@ else:
     sys.exit("Manual parameter choice not feasible")
 
 
-for traj in range(num_traj_updates):
+for ep in range(epochs):
 
+    print(f'Epoch {ep}, Update reference path')
     x_star = np.transpose(x_current_full)
+    
+    for it in range(40):
 
-    for patch in range(num_patches):
-        for it in range(20):
+        u_lower = torch.tensor([-a_max, -delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
+        u_upper = torch.tensor([a_max, delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
+        u_init= torch.tensor([0.1, 0.0]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(device)
 
-            # update batch size such that data points from a later patch have the same weighting as from an earlier
-            BS = BS*(patch+1)
+        x0 = utils_new.sample_init_traj_dist(BS, true_dx, x_star, num_patches)
+        
+        x0_diff = x0.clone().float()
 
-            u_lower = torch.tensor([-a_max, -delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
-            u_upper = torch.tensor([a_max, delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
-            u_init= torch.tensor([0.1, 0.0]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(device)
+        x0_buffer = sample_x0_from_buffer(BS, buffer_x0).detach()
 
-            x0 = utils_new.sample_init_traj_dist(BS, true_dx, x_star, num_patches)
+        x0_buffer_diff = x0_buffer.clone().float()
 
-            x0_diff = x0.clone().float()
+        progress_pred = torch.tensor(0.)
+        penalty_pred_d = torch.tensor(0.)
+        penalty_pred_v = torch.tensor(0.)
+        penalty_pred_phi = torch.tensor(0.)
 
-            x0_buffer = sample_x0_from_buffer(BS, buffer_x0).detach()
+        curv = utils_new.get_curve_hor_from_x(x0_diff, track_coord, mpc_H)
+        inp = torch.hstack((x0_diff[:,1:4], curv))
+        q_p_pred = model(inp)
 
-            x0_buffer_diff = x0_buffer.clone().float()
+        q, p = utils_new.q_and_p(mpc_T, q_p_pred, Q_manual, p_manual)
+        Q = torch.diag_embed(q, offset=0, dim1=-2, dim2=-1)
 
-            print(type(x0_diff[0,0]))
-            print(type(x0_buffer_diff[0,0]))
+        for sim in range(0, mpc_H//mpc_T):
 
-            progress_pred = torch.tensor(0.)
-            penalty_pred_d = torch.tensor(0.)
-            penalty_pred_v = torch.tensor(0.)
-            penalty_pred_phi = torch.tensor(0.)
+            pred_x, pred_u, pred_objs = mpc.MPC(
+                        true_dx.n_state, true_dx.n_ctrl, mpc_T,
+                        u_lower=u_lower, u_upper=u_upper, u_init=u_init,
+                        lqr_iter=lqr_iter,
+                        verbose=0,
+                        exit_unconverged=False,
+                        detach_unconverged=False,
+                        linesearch_decay=.8,
+                        max_linesearch_iter=4,
+                        grad_method=grad_method,
+                        eps=eps,
+                        n_batch=None,
+                    )(x0_diff, QuadCost(Q, p), true_dx)
 
-            curv = utils_new.get_curve_hor_from_x(x0_diff, track_coord, mpc_H)
-            inp = torch.hstack((x0_diff[:,1:4], curv))
-            q_p_pred = model(inp)
+            x0_diff = pred_x[-1].clone()
+            x0_diff[:,4] = x0_diff[:,0]
+            x0_diff[:,5] = 0.
 
-            q, p = utils_new.q_and_p(mpc_T, q_p_pred, Q_manual, p_manual)
-            Q = torch.diag_embed(q, offset=0, dim1=-2, dim2=-1)
-
-            for sim in range(0, mpc_H//mpc_T):
-
-                pred_x, pred_u, pred_objs = mpc.MPC(
-                            true_dx.n_state, true_dx.n_ctrl, mpc_T,
-                            u_lower=u_lower, u_upper=u_upper, u_init=u_init,
-                            lqr_iter=lqr_iter,
-                            verbose=0,
-                            exit_unconverged=False,
-                            detach_unconverged=False,
-                            linesearch_decay=.8,
-                            max_linesearch_iter=4,
-                            grad_method=grad_method,
-                            eps=eps,
-                            n_batch=None,
-                        )(x0_diff, QuadCost(Q, p), true_dx)
-
-                x0_diff = pred_x[-1].clone()
-                x0_diff[:,4] = x0_diff[:,0]
-                x0_diff[:,5] = 0.
-
-                progress_pred = progress_pred + pred_x[-1,:,5]
-                penalty_pred_d = penalty_pred_d + true_dx.penalty_d(pred_x[:,:,1])
-                penalty_pred_v = penalty_pred_v + true_dx.penalty_v(pred_x[:,:,3])
-                #import pdb
-                #pdb.set_trace()
+            progress_pred = progress_pred + pred_x[-1,:,5]
+            penalty_pred_d = penalty_pred_d + true_dx.penalty_d(pred_x[:,:,1])
+            penalty_pred_v = penalty_pred_v + true_dx.penalty_v(pred_x[:,:,3])
+            #import pdb
+            #pdb.set_trace()
 
 
+        progress = progress_pred
+        loss = -progress.mean() \
+        + 0.01*penalty_pred_d.sum(0).mean() \
+        + 0.01*penalty_pred_v.sum(0).mean()
 
-            progress = progress_pred
-            loss = -progress.mean() \
-            + penalty_pred_d.sum(0).mean() \
-            + penalty_pred_v.sum(0).mean()
-
-            opt.zero_grad()
-            loss.backward()
-            opt.step()
-
-
-            if it%5==0:
-            # V A L I D A T I O N   (only casadi)
-                with torch.no_grad():
-
-                    BS_val = 32
-
-                    # This sampling should bring always the same set of initial states
-                    x0_val = utils_new.sample_init(BS_val, true_dx, sn=0).numpy()
-
-                    x0_val_pred = x0_val[:,:6]
-                    x0_val_manual = x0_val[:,:6]
-
-                    progress_val_pred = 0.
-                    progress_val_manual = 0.
-
-                    for sim in range(mpc_H//mpc_T):
-
-                        x0_val_pred_torch = torch.tensor(x0_val_pred, dtype=torch.float32)
-                        curv_val = utils_new.get_curve_hor_from_x(x0_val_pred_torch, track_coord, mpc_H)
-                        inp_val = torch.hstack((x0_val_pred_torch[:,1:4], curv_val))
-                        q_p_pred_val = model(inp_val)
-                        q_val, p_val = utils_new.q_and_p(mpc_T, q_p_pred_val, Q_manual, p_manual)
-
-                        #print('Qd, Qs:', q_val[:,:,[1,5]].mean(0).mean(0).detach().numpy())
-                        #print('Pd, Ps:', p_val[:,:,[1,5]].mean(0).mean(0).detach().numpy())
-
-                        q_val_np_casadi = torch.permute(q_val[:,:,idx_to_casadi], (2, 1, 0)).detach().numpy()
-                        p_val_np_casadi = torch.permute(p_val[:,:,idx_to_casadi], (2, 1, 0)).detach().numpy()
-                        x_pred_val, u_pred_val = utils_new.solve_casadi_parallel(
-                            q_val_np_casadi, p_val_np_casadi,
-                            x0_val_pred, BS_val, dx, du, control)
+        #print('Loss terms: ',
+        #    round(progress.mean().detach().item(), 3), 
+        #    round(penalty_pred_d.sum(0).mean().detach().item(), 3), 
+        #    round(penalty_pred_v.sum(0).mean().detach().item(), 3))
+        
+        
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
 
 
-                        q_manual_casadi = np.expand_dims((Q_manual[:,idx_to_casadi].T), 1)
-                        p_manual_casadi = np.expand_dims((p_manual[:,idx_to_casadi].T), 1)
-                        x_manual, u_manual = utils_new.solve_casadi_parallel(
-                            np.repeat(q_manual_casadi, BS_val, 1),
-                            np.repeat(p_manual_casadi, BS_val, 1),
-                            x0_val_manual, BS_val, dx, du, control)
+        if it%10==0:
+            # L A P   P E R F O R M A N C E    (E V A L U A T I O N)
+            with torch.no_grad():
 
-                        x0_val_pred = x_pred_val[-1]
-                        x0_val_manual = x_manual[-1]
+                print('LAP PERFORMANCE:')
+                BS_test = 1
 
-                        x0_val_pred[:,4] = x_pred_val[-1,:,0]
-                        x0_val_manual[:,4] = x_manual[-1,:,0]
+                # This sampling should bring always the same set of initial states
+                x0_lap = utils_new.sample_init_test(BS_test, true_dx, sn=0).numpy()
 
-                        progress_val_pred = progress_val_pred + x_pred_val[-1,:,5]
-                        progress_val_manual = progress_val_manual + x_manual[-1,:,5]
+                x0_lap_pred = x0_lap[:,:6]
+                x0_lap_manual = x0_lap[:,:6]
 
-                        x0_val_pred[:,5] = 0.
-                        x0_val_manual[:,5] = 0.
+                finish_list = np.zeros((BS_test,))
+                lap_time_list = np.zeros((BS_test,))
 
+                for b in range(BS_test):
+                    finished = 0
+                    crashed = 0
+                    steps = 0
+                    max_steps=500
 
-                    progress_val = progress_val_pred - progress_val_manual
+                    x0_b_pred = x0_lap_pred[b].copy()
+                    x_pred_full = x0_b_pred.reshape(-1,1)
 
-                    if best_prog<progress_val.mean():
-                        torch.save(model.state_dict(), f'./saved_models/model_{str_model}.pkl')
+                    while finished==0 and crashed==0:
 
-                    print(f'{it}: Progress Diff: ', round(progress_val.mean(), 3),
-                          '\tProgress Pred: ', round(progress_val_pred.mean(), 3),
-                          '\tProgress Manual: ', round(progress_val_manual.mean(), 3)
-                         )
+                        x0_lap_pred_torch = torch.tensor(x0_b_pred, dtype=torch.float32).unsqueeze(0)
+                        curv_lap = utils_new.get_curve_hor_from_x(x0_lap_pred_torch, track_coord, mpc_H)
+                        inp_lap = torch.hstack((x0_lap_pred_torch[:,1:4], curv_lap))
+                        q_p_pred_lap = model(inp_lap)
+                        q_lap, p_lap = utils_new.q_and_p(mpc_T, q_p_pred_lap, Q_manual, p_manual)
 
-
-            if it%5==0:
-                # L A P   P E R F O R M A N C E    (E V A L U A T I O N)
-                with torch.no_grad():
-
-                    print('LAP PERFORMANCE:')
-                    BS_test = 1
-
-                    # This sampling should bring always the same set of initial states
-                    x0_lap = utils_new.sample_init_test(BS_test, true_dx, sn=0).numpy()
-
-                    x0_lap_pred = x0_lap[:,:6]
-                    x0_lap_manual = x0_lap[:,:6]
-
-                    finish_list = np.zeros((BS_test,))
-                    lap_time_list = np.zeros((BS_test,))
-
-                    for b in range(BS_test):
-                        finished = 0
-                        crashed = 0
-                        steps = 0
-                        max_steps=500
-
-                        x0_b_pred = x0_lap_pred[b].copy()
-                        x_pred_full = x0_b_pred.reshape(-1,1)
-
-                        while finished==0 and crashed==0:
-
-                            x0_lap_pred_torch = torch.tensor(x0_b_pred, dtype=torch.float32).unsqueeze(0)
-                            curv_lap = utils_new.get_curve_hor_from_x(x0_lap_pred_torch, track_coord, mpc_H)
-                            inp_lap = torch.hstack((x0_lap_pred_torch[:,1:4], curv_lap))
-                            q_p_pred_lap = model(inp_lap)
-                            q_lap, p_lap = utils_new.q_and_p(mpc_T, q_p_pred_lap, Q_manual, p_manual)
-
-                            q_lap_np_casadi = torch.permute(q_lap[:,:,idx_to_casadi], (2, 1, 0)).detach().numpy()
-                            p_lap_np_casadi = torch.permute(p_lap[:,:,idx_to_casadi], (2, 1, 0)).detach().numpy()
+                        q_lap_np_casadi = torch.permute(q_lap[:,:,idx_to_casadi], (2, 1, 0)).detach().numpy()
+                        p_lap_np_casadi = torch.permute(p_lap[:,:,idx_to_casadi], (2, 1, 0)).detach().numpy()
 
 
-                            x_b_pred, u_b_pred = utils_new.solve_casadi(
-                                q_lap_np_casadi[:,0,:], p_lap_np_casadi[:,0,:],
-                                x0_b_pred, dx, du, control)
+                        x_b_pred, u_b_pred = utils_new.solve_casadi(
+                            q_lap_np_casadi[:,0,:], p_lap_np_casadi[:,0,:],
+                            x0_b_pred, dx, du, control)
 
-                            x0_b_pred = x_b_pred[1]
-                            x_pred_full = np.append(x_pred_full, x0_b_pred.reshape(-1,1), axis=1)
+                        x0_b_pred = x_b_pred[1]
+                        x_pred_full = np.append(x_pred_full, x0_b_pred.reshape(-1,1), axis=1)
 
-                            if x0_b_pred[0]>track_coord[2].max().numpy():
-                                finished=1
+                        if x0_b_pred[0]>track_coord[2].max().numpy():
+                            finished=1
 
-                            if x0_b_pred[1]>0.17 or x0_b_pred[1]<-0.17 or steps>max_steps:
-                                crashed=1
+                        if x0_b_pred[1]>0.17 or x0_b_pred[1]<-0.17 or steps>max_steps:
+                            crashed=1
 
-                            steps = steps+1
+                        steps = steps+1
 
-                        lap_time = dt*steps
+                    lap_time = dt*steps
 
-                        print('current lap time: ', current_time)
-                        print('Pred lap time: ', lap_time)
+                    print('current lap time: ', current_time)
+                    print('Pred lap time: ', lap_time)
 
-                        if finished == 1 and lap_time < current_time:
-                            current_time = lap_time
-                            q_current = q_lap_np_casadi
-                            p_current = p_lap
-                            x_current_full = x_pred_full
+                    if finished == 1 and lap_time < current_time:
+                        current_time = lap_time
+                        q_current = q_lap_np_casadi
+                        p_current = p_lap
+                        x_current_full = x_pred_full
 
-                        # Compare with previous best lap_time and potentially replace parameter estimate
+                    # Compare with previous best lap_time and potentially replace parameter estimate
 
-                        finish_list[b] = finished
-                        lap_time_list[b] = lap_time
+                    finish_list[b] = finished
+                    lap_time_list[b] = lap_time
 
-                    print('Pred finish: ', finish_list)
-                    print('Pred lap time: ', lap_time_list)
-
-                    # We just compute the manual lap in the first iteration
-                    # if it==0:
-                    #     for b in range(BS_test):
-                    #         finished = 0
-                    #         crashed = 0
-                    #         steps = 0
-                    #         max_steps=500
-                    #
-                    #         x0_b_manual = x0_lap_manual[b].copy()
-                    #
-                    #         while finished==0 and crashed==0:
-                    #             q_lap_manual_casadi = Q_manual[:,idx_to_casadi].T
-                    #             p_lap_manual_casadi = p_manual[:,idx_to_casadi].T
-                    #
-                    #             x_b_manual, u_b_manual = utils_new.solve_casadi(
-                    #                 q_lap_manual_casadi, p_lap_manual_casadi,
-                    #                 x0_b_manual, dx, du, control)
-                    #
-                    #             x0_b_manual = x_b_manual[1]
-                    #
-                    #             if x0_b_manual[0]>track_coord[2].max().numpy():
-                    #                 finished=1
-                    #
-                    #             if x0_b_manual[1]>0.17 or x0_b_manual[1]<-0.17 or steps>max_steps:
-                    #                 crashed=1
-                    #
-                    #             steps = steps+1
-                    #
-                    #         lap_time = dt*steps
-                    #
-                    #         finish_list[b] = finished
-                    #         lap_time_list[b] = lap_time
-                    #
-                    #     print('Manual finish: ', finish_list)
-                    #     print('Manual lap time: ', lap_time_list)
+                print('Pred finish: ', finish_list)
