bcb5d2cb55cfa0df7973d088fa32c16cbb994bcd
Alan Lahoud <alan.lahoud@oru.se>
2024-10-13 12:41:12 +0200

    change loss function to imitate long term

--- utils_new.py
+++ utils_new.py
@@ -336,7 +336,7 @@ class SimpleNN(nn.Module):
         x = self.fc3(x)
         #x = self.output_activation(x) * self.K
         x = x.reshape(self.mpc_T, -1, self.O)
-        return x/5
+        return x
 
 def sample_init(BS, dyn, sn=None):
 
@@ -547,7 +547,7 @@ class FrenetKinBicycleDx(nn.Module):
 
         self.delta_max = params[6]
 
-        self.factor_pen = 500.
+        self.factor_pen = 50.
 
 
 
