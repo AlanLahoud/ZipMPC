2bcf1a7d92b176869a6f26e93b1ed7c01cfdc621
Alan Lahoud <alan.lahoud@oru.se>
2024-10-14 18:37:01 +0200

    lr

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -293,7 +293,7 @@ for ep in range(epochs):
         loss_delta = (u_true_torch[:mpc_T, :, 1] - pred_u[:, :, 1])**2
 
         # Ideal here would be to scale, but this is fine just to be in the same range
-        loss = 100*loss_dsigma.mean() + 100*loss_d.mean() + loss_v.mean() + loss_delta.mean()
+        loss = 10*loss_dsigma.mean() + 10*loss_d.mean() + loss_v.mean() + loss_delta.mean()
 
         #if it%5==0:
         #    print('Train loss:', 
@@ -353,7 +353,7 @@ for ep in range(epochs):
                 loss_delta_val = (u_true_val[:mpc_T, :, 1] - u_pred_val[:, :, 1])**2
         
                 # Ideal here would be to scale, but this is fine just to be in the same range
-                loss_val = 100*loss_dsigma_val.mean() + 100*loss_d_val.mean() + loss_v_val.mean() + loss_delta_val.mean()
+                loss_val = 10*loss_dsigma_val.mean() + 10*loss_d_val.mean() + loss_v_val.mean() + loss_delta_val.mean()
                 
                 print('Validation loss:', 
                       round(100*loss_dsigma_val.mean().item(), 5),
