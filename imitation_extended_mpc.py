671aa406de6ceedd63ed7a75dfd62dc0060f36b7
Alan Lahoud <alan.lahoud@oru.se>
2024-10-14 23:55:22 +0200

    change loss function to imitate long term

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -107,7 +107,7 @@ u0 = torch.tensor([0.0, 0.0])
 dx=4
 du=2
 
-BS = 64
+BS = 32
 u_lower = torch.tensor([-a_max, -delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
 u_upper = torch.tensor([a_max, delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
 u_init= torch.tensor([0.1, 0.0]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(device)
@@ -117,8 +117,8 @@ lqr_iter = 20
 grad_method = GradMethods.AUTO_DIFF
 
 model = utils_new.SimpleNN(mpc_H, n_Q, 3, max_p)
-#opt = torch.optim.Adam(model.parameters(), lr=0.0002, weight_decay=1e-5)
-opt = torch.optim.RMSprop(model.parameters(), lr=0.0005)
+opt = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=1e-5)
+#opt = torch.optim.RMSprop(model.parameters(), lr=0.0001)
 
 control = utils_new.CasadiControl(track_coord, params)
 Q_manual = np.repeat(np.expand_dims(np.array([0, 0.5, 0.5, 0, 0, 0, 0, 0, 0, 0]), 0), mpc_T, 0)
