b2d6af8fc9d794eafd0aadf78cd6879d7a8af87d
Alan Lahoud <alan.lahoud@oru.se>
2024-10-15 16:48:02 +0200

    back to adam

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -117,7 +117,7 @@ lqr_iter = 30
 grad_method = GradMethods.AUTO_DIFF
 
 model = utils_new.SimpleNN(mpc_H, n_Q, 6, max_p)
-opt = torch.optim.Adam(model.parameters(), lr=0.0004, weight_decay=1e-5)
+opt = torch.optim.Adam(model.parameters(), lr=0.0002, weight_decay=1e-5)
 #opt = torch.optim.RMSprop(model.parameters(), lr=0.0001)
 
 control = utils_new.CasadiControl(track_coord, params)
@@ -131,7 +131,7 @@ p_manual_H = np.repeat(np.expand_dims(np.array([0, 0, 0, 0, 0, -p_sigma_manual, 
 idx_to_casadi = [5,1,2,3,8,9]
 
 
-epochs = 10
+epochs = 18
 num_patches = 10
 BS_init = 40
 BS_val = 10
