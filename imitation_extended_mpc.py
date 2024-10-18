43f31263d55017fff91a1a04adae04d33d903ea3
Alan Lahoud <alan.lahoud@oru.se>
2024-10-18 10:52:16 +0200

    debug

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -111,13 +111,13 @@ BS = 32
 u_lower = torch.tensor([-a_max, -delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
 u_upper = torch.tensor([a_max, delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
 u_init= torch.tensor([0.1, 0.0]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(device)
-eps=0.01
-lqr_iter = 60
+eps=0.001
+lqr_iter = 80
 
 grad_method = GradMethods.AUTO_DIFF
 
 model = utils_new.SimpleNN(mpc_H, n_Q, 5, max_p)
-opt = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
+opt = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=1e-5)
 #opt = torch.optim.RMSprop(model.parameters(), lr=0.0001)
 
 control = utils_new.CasadiControl(track_coord, params)
