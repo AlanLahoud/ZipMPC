f4f6cbaaf44ea2d8af4fc3c2e046ce41e30158b9
Alan Lahoud <alan.lahoud@oru.se>
2024-10-18 15:46:15 +0200

    debug

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -312,9 +312,9 @@ for ep in range(epochs):
         diff_sigs = ((x_true_torch_S[:, :, 0] - pred_x[:, :, 0])**2).mean().item()
         print(diff_sigs)
 
-        if diff_sigs> 0.0001:
-            import pdb
-            pdb.set_trace()
+        #if diff_sigs> 0.0001:
+        #    import pdb
+        #    pdb.set_trace()
         
         if it%10==0:
             #import pdb
