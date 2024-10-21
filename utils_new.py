7afb5829d0218a5ca3c4a34974ed9979bf55a113
Alan Lahoud <alan.lahoud@oru.se>
2024-10-21 20:20:06 +0200

    dt 003

--- utils_new.py
+++ utils_new.py
@@ -603,7 +603,11 @@ def q_and_p(mpc_T, q_p_pred, Q_manual, p_manual):
     p[:,:,2] = p[:,:,2] + q_p_pred[:,:,4]
 
     #a
-    #q[:,:,8] = (q[:,:,1] + q_p_pred[:,:,6]).clamp(e + 0.5)
-    #p[:,:,8] = p[:,:,1] + q_p_pred[:,:,7]
+    q[:,:,8] = (q[:,:,8] + q_p_pred[:,:,5]).clamp(e)
+    p[:,:,8] = p[:,:,8] + q_p_pred[:,:,6]
+
+    #delta
+    q[:,:,9] = (q[:,:,9] + q_p_pred[:,:,7]).clamp(e)
+    p[:,:,9] = p[:,:,9] + q_p_pred[:,:,8]
 
     return q, p
