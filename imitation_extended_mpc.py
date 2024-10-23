15ce2de1fe7954a0e561ff89f984f3cf8dad1317
Alan Lahoud <alan.lahoud@oru.se>
2024-10-23 14:47:22 +0200

    cnn temporal

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -467,8 +467,12 @@ for ep in range(epochs):
                     lap_time_list[b] = lap_time
 
                 print(f'current lap time: {current_time} \t Pred lap time: {lap_time} \t Finished: {finished}')
-                print(x_pred_full[0,60], x_pred_full[0,90], x_pred_full[0,120], x_pred_full[0,150], x_pred_full[0,180])
-                print(x_manual_full_H[0,60], x_manual_full_H[0,90], x_manual_full_H[0,120], x_manual_full_H[0,150], x_manual_full_H[0,180])
+
+                try:
+                    print(x_pred_full[0,60], x_pred_full[0,90], x_pred_full[0,120], x_pred_full[0,150], x_pred_full[0,180])
+                    print(x_manual_full_H[0,60], x_manual_full_H[0,90], x_manual_full_H[0,120], x_manual_full_H[0,150], x_manual_full_H[0,180])
+                except:
+                    print('crash')
 
                 
                 if ep>5:
