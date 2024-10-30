870960ae1b669a195d31c3ec879b1ceef6cd4a74
Alan Lahoud <alan.lahoud@oru.se>
2024-10-30 11:00:57 +0100

    states imitation

--- utils_new.py
+++ utils_new.py
@@ -455,7 +455,7 @@ def sample_init(BS, dyn, sn=None):
     sigma_sample = torch.randint(int(0.0*di), int(14.5*di), (BS,1), generator=gen)/di
     d_sample = torch.randint(int(-0.06*di), int(0.06*di), (BS,1), generator=gen)/di
     phi_sample = torch.randint(int(-0.06*di), int(0.06*di), (BS,1), generator=gen)/di
-    v_sample = torch.randint(int(1.0*di), int(1.5*di), (BS,1), generator=gen)/di
+    v_sample = torch.randint(int(.5*di), int(1.8*di), (BS,1), generator=gen)/di
 
     sigma_diff_sample = torch.zeros((BS,1))
 
