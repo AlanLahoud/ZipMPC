3d75c76ff2ef491c5a9dc288b8f17460ccc3b2c5
Alan Lahoud <alan.lahoud@oru.se>
2024-10-30 11:58:40 +0100

    states imitation

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -127,7 +127,7 @@ if load_model==True:
     except:
         print('No model found to load')
         
-opt = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=1e-4)
+opt = torch.optim.Adam(model.parameters(), lr=0.00001, weight_decay=1e-4)
 #opt = torch.optim.RMSprop(model.parameters(), lr=0.00001)
 
 control = utils_new.CasadiControl(track_coord, params)
@@ -333,7 +333,7 @@ for ep in range(epochs):
         #print(diff_sigs)
 
         # Ideal here would be to scale
-        loss = 10*loss_dsigma[:,args_conv].sum(0).mean() + 10*loss_d[:,args_conv].sum(0).mean() + loss_phi[:,args_conv].sum(0).mean() \
+        loss = 10*loss_dsigma[:,args_conv].sum(0).mean() + 10*loss_d[:,args_conv].sum(0).mean() \ #+ loss_phi[:,args_conv].sum(0).mean() \
         + loss_v[:,args_conv].sum(0).mean() + 0.01*loss_a[:,args_conv].sum(0).mean() + 0.1*loss_delta[:,args_conv].sum(0).mean()
 
         #loss = 0.1*loss_a[:,args_conv].mean() + 0.1*loss_delta[:,args_conv].mean()
@@ -405,13 +405,13 @@ for ep in range(epochs):
                 loss_delta_val = (u_true_val[:mpc_T, :, 1] - u_pred_val[:, :, 1])**2
         
                 # Ideal here would be to scale, but this is fine just to be in the same range
-                loss_val = 10*loss_dsigma_val.sum(0).mean() + 10*loss_d_val.sum(0).mean() + loss_phi_val.sum(0).mean() \
+                loss_val = 10*loss_dsigma_val.sum(0).mean() + 10*loss_d_val.sum(0).mean() \ #+ loss_phi_val.sum(0).mean() \
                 + loss_v_val.sum(0).mean() + 0.01*loss_a_val.sum(0).mean() + 0.1*loss_delta_val.sum(0).mean()
                 
                 print('Validation loss:', 
                       round(10*loss_dsigma_val.sum(0).mean().item(), 5),
                       round(10*loss_d_val.sum(0).mean().item(), 5), 
-                      round(loss_phi_val.sum(0).mean().item(), 5), 
+                      #round(loss_phi_val.sum(0).mean().item(), 5), 
                       round(loss_v_val.sum(0).mean().item(), 5), 
                       round(0.01*loss_a_val.sum(0).mean().item(), 5), 
                       round(0.1*loss_delta_val.sum(0).mean().item(), 5), 
