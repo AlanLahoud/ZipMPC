b07d6630bca8a8a5898d0c75aba8d66461e601c9
Alan Lahoud <alan.lahoud@oru.se>
2024-11-06 13:51:48 +0100

    debug

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -122,7 +122,7 @@ model = utils_new.ImprovedNN(mpc_H, n_Q, 9, max_p)
 
 if load_model==True:
     try:
-        model.load_state_dict(torch.load(f'./saved_models/model_{str_model}.pkl'))
+        model.load_state_dict(torch.load(f'./saved_models/model_{str_model}_2.pkl'))
         print('Model loaded')
     except:
         print('No model found to load')
