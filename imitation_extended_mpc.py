80c016af16ed757a99568021961cb6c6dd98b298
Alan Lahoud <alan.lahoud@oru.se>
2024-11-07 21:30:00 +0100

    Qs fixed greater than 0

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -111,7 +111,7 @@ u0 = torch.tensor([0.0, 0.0])
 dx=4
 du=2
 
-BS = 48
+BS = 120
 u_lower = torch.tensor([-a_max, -delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
 u_upper = torch.tensor([a_max, delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
 u_init= torch.tensor([0.1, 0.0]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(device)
@@ -131,7 +131,7 @@ if load_model==True:
         
 #opt = torch.optim.Adam(model.parameters(), lr=0.0005, weight_decay=1e-5)
 #opt = torch.optim.RMSprop(model.parameters(), lr=0.0001)
-opt = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
+opt = torch.optim.AdamW(model.parameters(), lr=3e-5, weight_decay=1e-5)
 
 control = utils_new.CasadiControl(track_coord, params)
 Q_manual = np.repeat(np.expand_dims(np.array([0.0, 1.0, 1.0, 0.0, 0, 0, 0, 0, 0.1, 0.1]), 0), mpc_T, 0)
