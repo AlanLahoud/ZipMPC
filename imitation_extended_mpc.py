f5347f29c46743670788e9db6241749ee4396a07
Alan Lahoud <alan.lahoud@oru.se>
2024-11-09 10:09:47 +0100

    Qs fixed greater than 0

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -348,7 +348,7 @@ for ep in range(epochs):
         #print(diff_sigs)
 
         # Ideal here would be to scale
-        loss = 100*loss_dsigma[:,args_conv].sum(0).mean() + 100*loss_d[:,args_conv].sum(0).mean() + 10*loss_v[:,args_conv].sum(0).mean() + 0.01*loss_a[:,args_conv].sum(0).mean() + 0.1*loss_delta[:,args_conv].sum(0).mean()
+        loss = 100*loss_dsigma[:,args_conv].sum(0).mean() + 1000*loss_d[:,args_conv].sum(0).mean() + 10*loss_v[:,args_conv].sum(0).mean() + 0.01*loss_a[:,args_conv].sum(0).mean() + 1*loss_delta[:,args_conv].sum(0).mean()
 
         #loss = loss_a[:,args_conv].sum(0).mean() + 10*loss_delta[:,args_conv].sum(0).mean()
 
@@ -423,17 +423,17 @@ for ep in range(epochs):
                 loss_delta_val = (u_true_val[:mpc_T, :, 1] - u_pred_val[:, :, 1])**2
         
                 # Ideal here would be to scale, but this is fine just to be in the same range
-                loss_val = 100*loss_dsigma_val.sum(0).mean() + 100*loss_d_val.sum(0).mean() + 10*loss_v_val.sum(0).mean() + 0.01*loss_a_val.sum(0).mean() + 0.1*loss_delta_val.sum(0).mean()
+                loss_val = 100*loss_dsigma_val.sum(0).mean() + 1000*loss_d_val.sum(0).mean() + 10*loss_v_val.sum(0).mean() + 0.01*loss_a_val.sum(0).mean() + 1*loss_delta_val.sum(0).mean()
 
                 print('Train loss:', loss_train_avg)
                 
                 print('Validation loss:', 
                       round(100*loss_dsigma_val.sum(0).mean().item(), 5),
-                      round(100*loss_d_val.sum(0).mean().item(), 5), 
+                      round(1000*loss_d_val.sum(0).mean().item(), 5), 
                       #round(loss_phi_val.sum(0).mean().item(), 5), 
                       round(10*loss_v_val.sum(0).mean().item(), 5), 
                       round(0.01*loss_a_val.sum(0).mean().item(), 5), 
-                      round(0.1*loss_delta_val.sum(0).mean().item(), 5), 
+                      round(1*loss_delta_val.sum(0).mean().item(), 5), 
                       round(loss_val.item(), 5))
 
             # L A P   P E R F O R M A N C E    (E V A L U A T I O N)
