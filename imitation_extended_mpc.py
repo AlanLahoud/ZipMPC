7e59c10fedd382d1b052b6d4a96b8f5bdb7866a9
Alan Lahoud <alan.lahoud@oru.se>
2024-11-10 22:16:14 +0100

    smoothing q

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -283,6 +283,8 @@ for ep in range(epochs):
         
         x0_1 = utils_new.sample_init_traj_dist(BS//2, true_dx, x_star, npat, sn=0)
         x0_2 = utils_new.sample_init_traj_dist(BS//2, true_dx, np.transpose(x_manual_full_H), npat, sn=0)
+        import pdb
+        pdb.set_trace()
         #x0_3 = utils_new.sample_init(BS - BS//3 - BS//3, true_dx)
 
         #x0 = torch.vstack((x0_1, x0_2, x0_3))
