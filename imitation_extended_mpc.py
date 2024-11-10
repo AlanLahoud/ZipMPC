f73b75f8822ba73daa530336b86b97047d41ac45
Alan Lahoud <alan.lahoud@oru.se>
2024-11-10 22:25:21 +0100

    smoothing q

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -117,8 +117,8 @@ BS = 60
 u_lower = torch.tensor([-a_max, -delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
 u_upper = torch.tensor([a_max, delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
 u_init= torch.tensor([0.1, 0.0]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(device)
-eps=0.001
-lqr_iter = 25
+eps=0.00001
+lqr_iter = 35
 
 grad_method = GradMethods.AUTO_DIFF
 
