d5796a617240e81c9937738b1e4080699e55d493
Alan Lahoud <alan.lahoud@oru.se>
2024-11-10 22:30:55 +0100

    smoothing q

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -330,7 +330,7 @@ for ep in range(epochs):
                     verbose=0,
                     exit_unconverged=False,
                     detach_unconverged=False,
-                    linesearch_decay=.3,
+                    linesearch_decay=.1,
                     max_linesearch_iter=4,
                     grad_method=grad_method,
                     eps=eps,
