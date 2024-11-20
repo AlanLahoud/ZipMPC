e6ecbde5c32cbaabdca557c9c746c49ca270a54b
Alan Lahoud <alan.lahoud@oru.se>
2024-11-20 11:34:19 +0100

    debug unalignment paj

--- imitation_extended_mpc_paj_2.py
+++ imitation_extended_mpc_paj_2.py
@@ -330,10 +330,10 @@ for ep in range(epochs):
         loss_delta = (u_true_torch[:mpc_L, :, 1] - pred_u[:mpc_L, :, 1])**2
 
         diff_shorts = ((x_true_torch_S[:mpc_L, :, 2] - pred_x[:mpc_L, :, 2])**2).sum(0)
-        args_conv = torch.argwhere(diff_shorts<0.00001)
+        args_conv = torch.argwhere(diff_shorts<0.001)
 
-        import pdb
-        pdb.set_trace()
+        #import pdb
+        #pdb.set_trace()
         #print(diff_sigs)
 
         # Ideal here would be to scale
