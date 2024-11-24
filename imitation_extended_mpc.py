45a30976ccdcfc3733f67aab88715af8bc5421b3
Alan Lahoud <alan.lahoud@oru.se>
2024-11-24 01:44:27 +0100

    mpcL

--- imitation_extended_mpc.py
+++ imitation_extended_mpc.py
@@ -288,8 +288,8 @@ for ep in range(epochs):
         #if ep+2 < npat:
         #    npat = ep + 2
         
-        x0 = utils_new.sample_init_traj_dist(BS, true_dx, x_star, npat).float()
-        #x0_2 = utils_new.sample_init_traj_dist(BS//3, true_dx, np.transpose(x_manual_full_H), npat)
+        #x0 = utils_new.sample_init_traj_dist(BS, true_dx, x_star, npat).float()
+        x0 = utils_new.sample_init_traj_dist(BS, true_dx, np.transpose(x_manual_full_H), npat)
         #x0_3 = utils_new.sample_init(BS//3, true_dx)
 
         #x0 = torch.vstack((x0_1, x0_2, x0_3))
