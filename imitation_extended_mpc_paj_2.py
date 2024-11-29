c3309c05eecaf1e5a5b5b4128a07c5a6bf7fd243
Alan Lahoud <alan.lahoud@oru.se>
2024-11-29 15:23:27 +0100

    conv pac

--- imitation_extended_mpc_paj_2.py
+++ imitation_extended_mpc_paj_2.py
@@ -115,7 +115,7 @@ u_lower = torch.tensor([-a_max, -delta_max]).unsqueeze(0).unsqueeze(0).repeat(mp
 u_upper = torch.tensor([a_max, delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
 u_init= torch.tensor([0.1, 0.0]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(device)
 eps=0.001
-lqr_iter = 35
+lqr_iter = 30
 
 grad_method = GradMethods.AUTO_DIFF
 
@@ -125,11 +125,11 @@ model = utils_new.TCN(mpc_H, n_Q, 2, max_p)
 opt = torch.optim.AdamW(model.parameters(), lr=1e-6, weight_decay=1e-4)
 
 control = utils_new.CasadiControl(track_coord, params)
-Q_manual = np.repeat(np.expand_dims(np.array([0, 3.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 10, 10, 0.0, 0.2]), 0), mpc_T, 0)
+Q_manual = np.repeat(np.expand_dims(np.array([0, 3.0, 0.5, 0.01, 0.01, 0.01, 0.01, 0.01, 1, 1, 0.01, 0.5]), 0), mpc_T, 0)
 p_manual = np.repeat(np.expand_dims(np.array([0, 0, 0, 0, 0., 0, 0, -p_sigma_manual, 0, 0, 0, 0]), 0), mpc_T, 0)
 
 control_H = utils_new.CasadiControl(track_coord, params_H)
-Q_manual_H = np.repeat(np.expand_dims(np.array([0, 3.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 10, 10, 0.0, 0.2]), 0), mpc_H, 0)
+Q_manual_H = np.repeat(np.expand_dims(np.array([0, 3.0, 0.5, 0.01, 0.01, 0.01, 0.01, 0.01, 1, 1, 0.01, 0.5]), 0), mpc_H, 0)
 p_manual_H = np.repeat(np.expand_dims(np.array([0, 0, 0, 0, 0., 0, 0, -p_sigma_manual, 0, 0, 0, 0]), 0), mpc_H, 0)
 
 idx_to_casadi = [7,1,2,3,4,5,10,11]
@@ -326,7 +326,7 @@ for ep in range(epochs):
                     verbose=0,
                     exit_unconverged=False,
                     detach_unconverged=False,
-                    linesearch_decay=.7,
+                    linesearch_decay=.3,
                     max_linesearch_iter=60,
                     grad_method=grad_method,
                     eps=eps,
