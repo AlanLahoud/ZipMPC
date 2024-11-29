e4d183f301f7312d5ef900ced9e215682fa92f50
Alan Lahoud <alan.lahoud@oru.se>
2024-11-29 16:13:50 +0100

    conv pac

--- imitation_extended_mpc_paj_2.py
+++ imitation_extended_mpc_paj_2.py
@@ -110,7 +110,7 @@ u0 = torch.tensor([0.0, 0.0])
 dx=6
 du=2
 
-BS = 40
+BS = 80
 u_lower = torch.tensor([-a_max, -delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
 u_upper = torch.tensor([a_max, delta_max]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(dev)
 u_init= torch.tensor([0.1, 0.0]).unsqueeze(0).unsqueeze(0).repeat(mpc_T, BS, 1)#.to(device)
@@ -122,7 +122,7 @@ grad_method = GradMethods.AUTO_DIFF
 model = utils_new.TCN(mpc_H, n_Q, 2, max_p)
 #opt = torch.optim.Adam(model.parameters(), lr=0.00005, weight_decay=1e-3)
 #opt = torch.optim.RMSprop(model.parameters(), lr=0.0001)
-opt = torch.optim.AdamW(model.parameters(), lr=1e-6, weight_decay=1e-4)
+opt = torch.optim.AdamW(model.parameters(), lr=5e-6, weight_decay=1e-4)
 
 control = utils_new.CasadiControl(track_coord, params)
 Q_manual = np.repeat(np.expand_dims(np.array([0, 3.0, 0.5, 0.01, 0.01, 0.01, 0.01, 0.01, 1, 1, 0.01, 0.5]), 0), mpc_T, 0)
@@ -283,10 +283,12 @@ for ep in range(epochs):
         #if ep+2 < npat:
         #    npat = ep + 2
 
-        x0 = utils_new.sample_init_traj_dist_dyn(BS, true_dx, x_star, npat).float()
+        x0_1 = utils_new.sample_init_traj_dist_dyn(BS//2, true_dx, x_star, npat).float()
+        x0_2 = utils_new.sample_init_dyn(BS//2, true_dx).float()
+        
         #x0_2 = utils_new.sample_init_traj_dist_dyn(BS//2, true_dx, np.transpose(x_manual_full_H), npat).float()
 
-        #x0 = torch.vstack((x0_1, x0_2))
+        x0 = torch.vstack((x0_1, x0_2))
 
         #x0 = utils_new.sample_init_test_dyn(BS, true_dx).float()
 
