83d34e4d42f876c308b54aba0a5cd2e694bc13d9
Alan Lahoud <alan.lahoud@oru.se>
2024-12-13 19:18:13 +0100

    rewriting scripts

--- reduce_horizon_mpc.py
+++ reduce_horizon_mpc.py
@@ -202,7 +202,7 @@ u_init_val = torch.tensor([a_max, 0.0]).unsqueeze(0).unsqueeze(0).repeat(NS, BS_
 ################### M O D E L  &  T R A I N ##############################################
 ##########################################################################################
 
-model = utils.TCN2(NL, n_Q, 5, max_p)
+model = utils.TCN(NL, n_Q, 5, max_p)
 opt = torch.optim.AdamW(model.parameters(), lr=2e-4, weight_decay=1e-4)
 
 its_per_epoch = 40
