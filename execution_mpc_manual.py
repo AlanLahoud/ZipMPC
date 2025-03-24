a10ea817d2849c494a24f921c98b57dcc4d263ff
Alan Lahoud <alan.lahoud@oru.se>
2025-03-24 17:21:33 +0100

    exec pac

--- execution_mpc_manual.py
+++ execution_mpc_manual.py
@@ -225,7 +225,7 @@ print(f'Manual extended NL = {NL}, lap time: {lap_time}, finished: {finished}')
 
 
 plot_sim(torch.tensor(x_frenet_full), torch.tensor(x_frenet_full), np.array(u_full)[:,1], 
-         f'./outs_imgs/{out_path}.png', lab_text='Steering', time_lap=lap_time)
+         f'./outs_imgs/steer_{out_path}.png', lab_text='Steering', time_lap=lap_time)
 
 plot_sim(torch.tensor(x_frenet_full), torch.tensor(x_frenet_full), np.array(x_frenet_full)[:,4], 
-         f'./outs_imgs/{out_path}.png', lab_text='Velocity', time_lap=lap_time)
+         f'./outs_imgs/vel_{out_path}.png', lab_text='Velocity', time_lap=lap_time)
