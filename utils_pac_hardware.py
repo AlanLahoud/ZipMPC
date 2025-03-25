d070dcd172ee9680ab4ba8997bc0b5111031ac58
Alan Lahoud <alan.lahoud@oru.se>
2025-03-25 09:48:03 +0100

    hard

--- utils_pac_hardware.py
+++ utils_pac_hardware.py
@@ -551,8 +551,8 @@ class CasadiControl():
         #import pdb
         #pdb.set_trace()
 
-        barr1 = -log(fmax(1e-5, x_sym[1,0:N+1] + 0.48*self.max_track_width_perc*self.track_width*np.ones((1,N+1))))
-        barr2 = -log(fmax(1e-5, 0.48*self.max_track_width_perc*self.track_width*np.ones((1,N+1)) - x_sym[1,0:N+1]))
+        barr1 = -log(fmax(1e-5, x_sym[1,0:N+1] + 0.47*self.max_track_width_perc*self.track_width*np.ones((1,N+1))))
+        barr2 = -log(fmax(1e-5, 0.47*self.max_track_width_perc*self.track_width*np.ones((1,N+1)) - x_sym[1,0:N+1]))
         
         barrier = (1/N)* (barr1 + barr2)
 
