839b6ef2579d768fd69601d8fe8e13144fe3784b
Alan Lahoud <alan.lahoud@oru.se>
2025-03-26 09:49:49 +0100

    hard

--- utils_pac_hardware.py
+++ utils_pac_hardware.py
@@ -581,7 +581,7 @@ class CasadiControl():
                     -self.a_max*np.ones(N),
                     -self.delta_max*np.ones(N),
                     -0.5*self.max_track_width_perc*self.track_width*np.ones(N+1),
-                    0.1*np.ones(N+1)]
+                    0.05*np.ones(N+1)]
 
         ubg = np.r_[np.zeros(N+1),
                     np.zeros(N+1),
