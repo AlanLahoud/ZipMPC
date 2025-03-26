c2248c10b09ec89d92e588ed6fe32e6a9ad078e3
Alan Lahoud <alan.lahoud@oru.se>
2025-03-26 09:48:28 +0100

    hard

--- utils_pac_hardware.py
+++ utils_pac_hardware.py
@@ -554,7 +554,7 @@ class CasadiControl():
         barr1 = -log(fmax(1e-5, x_sym[1,0:N+1] + 0.47*self.max_track_width_perc*self.track_width*np.ones((1,N+1))))
         barr2 = -log(fmax(1e-5, 0.47*self.max_track_width_perc*self.track_width*np.ones((1,N+1)) - x_sym[1,0:N+1]))
         
-        barrier = 5*(1/N)* (barr1 + barr2)
+        barrier = 10*(1/N)* (barr1 + barr2)
 
         
         l = sum2(sum1(0.5*q_sym*feat*feat + p_sym*feat)) + sum2(sum1(barrier))
